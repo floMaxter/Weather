a3bbd5de6029cd1d7d25769a364881f5c8a3f10b
Max Vdovin <maxpostupaet@gmail.com>
2025-04-19 14:15:49 +0300

    fix: Change the field name from username to login

--- src/main/java/com/projects/weather/model/User.java
+++ src/main/java/com/projects/weather/model/User.java
@@ -24,7 +24,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    private String login;
 
     private String password;
 }
